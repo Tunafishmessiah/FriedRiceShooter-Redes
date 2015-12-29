79709d8122d33102bc4447ff0d68dd943225647e
Pedro Miguel Leal de Carvalho <tunafishmessiah@gmail.com>
2015-12-29 21:18:51 +0000

    Starting Shit Up

--- FriedRiceShooter/ClientManager.cs
+++ FriedRiceShooter/ClientManager.cs
@@ -7,6 +7,7 @@ using System.Net.Sockets;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 
 namespace FriedRiceShooter
 {
@@ -20,7 +21,6 @@ namespace FriedRiceShooter
 
         //Definir variaveis e respetivos Locks
         public Dictionary<IPEndPoint, Ship> enemies = new Dictionary<IPEndPoint, Ship>();
-        private List<Message> MessagesToSend;
         
         //Locks
         private Object ShipsLock = new Object();
@@ -32,7 +32,28 @@ namespace FriedRiceShooter
             this.playah = player;
             this.Me = new UdpClient(9999);
 
+            byte[] message = new byte[1024];
+            //ServidorEndPoint
             IPEndPoint origin = new IPEndPoint(IPAddress.Any, 0);
+            //Para usar como debugger
+            IPEndPoint DoubleOrigin = new IPEndPoint(IPAddress.Parse("127.0.0.1"),9919);
+
+            Thread ConnectMe = new Thread(() => {
+                //Inserir Comunicação com o servidor aqui
+                //Por enquanto vai ser só para adicionar um novo player do qual eu sei o IP
+                
+                message = Encoding.ASCII.GetBytes("HiMate");
+
+
+
+                Socket clientConnectionSocketSend = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                clientConnectionSocketSend.SendTo(message, DoubleOrigin);
+                Thread.Sleep(100);
+                /*
+                 * 
+                 */
+
+            });
 
             //Making it able to listen
             Me.Client.EnableBroadcast = true;
@@ -41,6 +62,8 @@ namespace FriedRiceShooter
             //Giving it some time do do his thing
             Me.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.SendBuffer, 150000);
             Me.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveBuffer, 150000);
+
+            //Debuging part
         }
 
         public void Update(GameTime gameTime, Player player)
@@ -49,25 +72,6 @@ namespace FriedRiceShooter
 
             if (enemies.Count > 0)
             {
-
-                foreach (IPEndPoint enemy in enemies.Keys)
-                {
-                    Message position = new Message("PX" + player.Position.X.ToString() + "Y" + player.Position.Y.ToString(), enemy);  
-                }
-                if (MessagesToSend.Count > 0)
-                {
-                    lock (MessageLock)
-                    {
-                        foreach (Message mess in MessagesToSend.ToArray())
-                        {
-                            Me.BeginSend(mess.message,
-                                mess.message.Length,
-                                mess.receiver,
-                                terminateSend,
-                                Me);
-                        }
-                    }
-                }
                 
             }
 
@@ -79,6 +83,13 @@ namespace FriedRiceShooter
             me.EndSend(result);
         }
 
+        private void AddEnemy( string IP )
+        {
+            Ship enemy = new Player(Vector2.One, playah.graphics, playah.texture, playah.BulletTexture, playah.Spriter);
+            IPEndPoint newIP = new IPEndPoint(IPAddress.Parse(IP), 1000);
+
+            enemies.Add(newIP, enemy);
+        }
 
     }
 }
